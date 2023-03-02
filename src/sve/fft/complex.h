5fab7dbb3e11c4757f11d11768b69fff39e6c9ec
antfor <38752136+antfor@users.noreply.github.com>
2023-03-02 18:26:22 +0100

    cmla does not work?

--- src/sve/fft/complex.h
+++ src/sve/fft/complex.h
@@ -141,6 +141,144 @@ static inline void fft8(
 
 }
 
+static inline void fft8_new(
+    const float t[restrict static 16],
+    float f[restrict static 16])
+{
+    
+    const float f8t1r[8] = {1.0f,  SQRT2_OVER_2, 0.0f, -SQRT2_OVER_2, -1.0f, -SQRT2_OVER_2, 0.0f,  SQRT2_OVER_2};
+    const float f8t3r[8] = {1.0f, -SQRT2_OVER_2, 0.0f,  SQRT2_OVER_2, -1.0f,  SQRT2_OVER_2, 0.0f, -SQRT2_OVER_2};
+    const float f8t5r[8] = {1.0f, -SQRT2_OVER_2, 0.0f,  SQRT2_OVER_2, -1.0f,  SQRT2_OVER_2, 0.0f, -SQRT2_OVER_2};
+    const float f8t7r[8] = {1.0f,  SQRT2_OVER_2, 0.0f, -SQRT2_OVER_2, -1.0f, -SQRT2_OVER_2, 0.0f,  SQRT2_OVER_2};
+
+    const float f8t2r[8] = {1.0f,  0.0f,                 -1.0f,  0.0f,                  1.0f,  0.0f,                  -1.0f, 0.0f};
+    const float f8t4r[8] = {1.0f, -1.0f,                  1.0f, -1.0f,                  1.0f, -1.0f,                   1.0f,-1.0f};
+    const float f8t6r[8] = {1.0f,  0.0f,                 -1.0f,  0.0f,                  1.0f,  0.0f,                  -1.0f, 0.0f};
+
+    const float f8t1i[8] = {0.0f, -SQRT2_OVER_2, -1.0f,  -SQRT2_OVER_2,0.0f, SQRT2_OVER_2, 1.0f,  SQRT2_OVER_2};
+    const float f8t3i[8] = {0.0f, -SQRT2_OVER_2,  1.0f,  -SQRT2_OVER_2,0.0f, SQRT2_OVER_2, -1.0f, SQRT2_OVER_2};
+    const float f8t5i[8] = {0.0f,  SQRT2_OVER_2, -1.0f,   SQRT2_OVER_2,0.0f,-SQRT2_OVER_2, 1.0f,  -SQRT2_OVER_2};
+    const float f8t7i[8] = {0.0f,  SQRT2_OVER_2,  1.0f,   SQRT2_OVER_2,0.0f,-SQRT2_OVER_2, -1.0f, -SQRT2_OVER_2};
+
+    const float f8t2i[8] = {0.0f, -1.0f,                0.0f,  1.0f,                  0.0f, -1.0f,                   0.0f, 1.0f};
+    const float f8t4i[8] = {0.0f,  0.0f,                0.0f,  0.0f,                  0.0f,  0.0f,                   0.0f, 0.0f};
+    const float f8t6i[8] = {0.0f,  1.0f,                0.0f, -1.0f,                  0.0f,  1.0f,                   0.0f,-1.0f};
+
+
+    svfloat32_t f0r, f1r ,f2r, f3r, f4r, f5r, f6r, f7r,f0i, f1i ,f2i, f3i, f4i, f5i, f6i, f7i;
+    f0r = svdup_f32(0.0f);
+    f1r = svdup_f32(0.0f);
+    f2r = svdup_f32(0.0f);
+    f3r = svdup_f32(0.0f);
+    f4r = svdup_f32(0.0f);
+    f5r = svdup_f32(0.0f);
+    f6r = svdup_f32(0.0f);
+    f7r = svdup_f32(0.0f);
+
+    f0i = svdup_f32(0.0f);
+    f1i = svdup_f32(0.0f);
+    f2i = svdup_f32(0.0f);
+    f3i = svdup_f32(0.0f);
+    f4i = svdup_f32(0.0f);
+    f5i = svdup_f32(0.0f);
+    f6i = svdup_f32(0.0f);
+    f7i = svdup_f32(0.0f);
+
+    svfloat32_t f1tr, f1ti, f2tr, f2ti, f3tr, f3ti, f4tr, f4ti, f5tr, f5ti, f6tr, f6ti, f7tr, f7ti;
+    svbool_t pg;
+    const svbool_t all_active = svptrue_b32();
+
+    uint64_t numVals = svlen(f0r);
+
+    for (uint32_t i = 0; i < 8; i += numVals)
+    {
+        pg = svwhilelt_b32_s32(i, 8);
+      
+
+        f0r = svld1(pg, t + i);
+        f0i = svld1(pg, t + i + 8);
+
+        f1tr = svld1(pg, f8t1r + i);
+        f1ti = svld1(pg, f8t1i + i);
+        f1r = svmad_m(pg, f0r, f1tr, f1r);
+        f1r = svmsb_m(pg, f0i, f1ti, f1r);
+        f1i = svmad_m(pg, f0r, f1ti, f1i);
+        f1i = svmad_m(pg, f0i, f1tr, f1i);
+        
+
+        f2tr = svld1(pg, f8t2r + i);
+        f2ti = svld1(pg, f8t2i + i);
+        f2r = svmad_m(pg, f0r, f2tr, f2r);
+        f2r = svmsb_m(pg, f0i, f2ti, f2r);
+        f2i = svmad_m(pg, f0r, f2ti, f2i);
+        f2i = svmad_m(pg, f0i, f2tr, f2i);
+
+
+        f3tr = svld1(pg, f8t3r + i);
+        f3ti = svld1(pg, f8t3i + i);
+        f3r = svmad_m(pg, f0r, f3tr, f3r);
+        f3r = svmsb_m(pg, f0i, f3ti, f3r);
+        f3i = svmad_m(pg, f0r, f3ti, f3i);
+        f3i = svmad_m(pg, f0i, f3tr, f3i);
+
+        f4tr = svld1(pg, f8t4r + i);
+        f4ti = svld1(pg, f8t4i + i);
+        f4r = svmad_m(pg, f0r, f4tr, f4r);
+        f4r = svmsb_m(pg, f0i, f4ti, f4r);
+        f4i = svmad_m(pg, f0r, f4ti, f4i);
+        f4i = svmad_m(pg, f0i, f4tr, f4i);
+
+
+        f5tr = svld1(pg, f8t5r + i);
+        f5ti = svld1(pg, f8t5i + i);
+        f5r = svmad_m(pg, f0r, f5tr, f5r);
+        f5r = svmsb_m(pg, f0i, f5ti, f5r);
+        f5i = svmad_m(pg, f0r, f5ti, f5i);
+        f5i = svmad_m(pg, f0i, f5tr, f5i);
+
+        f6tr = svld1(pg, f8t6r + i);
+        f6ti = svld1(pg, f8t6i + i);
+        f6r = svmad_m(pg, f0r, f6tr, f6r);
+        f6r = svmsb_m(pg, f0i, f6ti, f6r);
+        f6i = svmad_m(pg, f0r, f6ti, f6i);
+        f6i = svmad_m(pg, f0i, f6tr, f6i);
+
+
+        f7tr = svld1(pg, f8t7r + i);
+        f7ti = svld1(pg, f8t7i + i);
+        f7r = svmad_m(pg, f0r, f7tr, f7r);
+        f7r = svmsb_m(pg, f0i, f7ti, f7r);
+        f7i = svmad_m(pg, f0r, f7ti, f7i);
+        f7i = svmad_m(pg, f0i, f7tr, f7i);
+
+    }
+
+    f[0] = svaddv(all_active, f0r);
+    f[1] = svaddv(all_active, f0i);
+
+    f[2] = svaddv(all_active, f1r);
+    f[3] = svaddv(all_active, f1i);
+
+    f[4] = svaddv(all_active, f2r);
+    f[5] = svaddv(all_active, f2i);
+
+    f[6] = svaddv(all_active, f3r);
+    f[7] = svaddv(all_active, f3i);
+
+    f[8] = svaddv(all_active, f4r);
+    f[9] = svaddv(all_active, f4i);
+
+    f[10] = svaddv(all_active, f5r);
+    f[11] = svaddv(all_active, f5i);
+
+    f[12] = svaddv(all_active, f6r);
+    f[13] = svaddv(all_active, f6i);
+
+    f[14] = svaddv(all_active, f7r);
+    f[15] = svaddv(all_active, f7i);
+
+}
+
 
 inline static void fft16(
     const float t[restrict static 32],
