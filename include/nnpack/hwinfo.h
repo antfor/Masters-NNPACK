9a52758f319cf7aae49a20765ef0964e31229a13
antfor <38752136+antfor@users.noreply.github.com>
2023-05-24 17:09:14 +0200

    fixed gemm_FFT16 in Hwinfo

--- include/nnpack/hwinfo.h
+++ include/nnpack/hwinfo.h
@@ -159,7 +159,7 @@ struct cxgemm {
 	nnp_full_tuple_gemm_function s4cX_conjb_upto_mr_x_nr;
 	nnp_fast_tuple_gemm_function cX_conjb_only_mr_x_nr;
 	nnp_full_tuple_gemm_function cX_conjb_upto_mr_x_nr;
-	#if NNP_BACKEND_SVE
+	#if NNP_BACKEND_SVE || NNP_BACKEND_RISCV
 	nnp_fast_tuple_gemm_function s4cX_conjb_only_mr_x_nr_FFT16;
 	nnp_full_tuple_gemm_function s4cX_conjb_upto_mr_x_nr_FFT16;
 	nnp_fast_tuple_gemm_function cX_conjb_only_mr_x_nr_FFT16;
